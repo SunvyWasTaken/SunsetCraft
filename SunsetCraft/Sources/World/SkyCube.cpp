0d1dbe72b070f24b2a2487213e385cec6bc05c9c
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-02-20 09:02:44 +0100

    start of the cloud

--- SunsetCraft/Sources/World/SkyCube.cpp
+++ SunsetCraft/Sources/World/SkyCube.cpp
@@ -66,38 +66,85 @@ namespace
 
         return indices;
     }
+
+    constexpr float size = 4000.f;
+    constexpr float height = 150.f;
+
+    std::array<glm::vec3, 4> CloudVertices = {
+        glm::vec3{-size, height, -size},
+        glm::vec3{size, height, -size},
+        glm::vec3{size, height, size},
+        glm::vec3{-size, height, size}
+    };
+
+    std::vector<uint32_t> CloudIndices = {
+        0,1,2,
+        0,2,3
+    };
+
+    std::unique_ptr<SunsetEngine::Drawable> CloudDrawable = nullptr;
 }
 
 SkyCube::SkyCube()
     : m_Drawable(std::make_unique<SunsetEngine::Drawable>())
 {
     LOG("SunsetCraft", info, "SkyDome generated")
-    auto shader = std::make_shared<SunsetEngine::Shader>("SunsetCraft/Shaders/SkyCube.vert", "SunsetCraft/Shaders/SkyCube.frag");
-    m_Drawable->m_Material->m_Shader = shader;
+    {
+        auto shader = std::make_shared<SunsetEngine::Shader>("SunsetCraft/Shaders/SkyCube.vert", "SunsetCraft/Shaders/SkyCube.frag");
+        m_Drawable->m_Material->m_Shader = shader;
 
-    std::vector<Vertex> vertices = GenerateSkyDome(500.f, 64, 32);
-    std::vector<uint32_t> indices = GenerateSkyDomeIndices(64, 32);
+        std::vector<Vertex> vertices = GenerateSkyDome(500.f, 64, 32);
+        std::vector<uint32_t> indices = GenerateSkyDomeIndices(64, 32);
 
-    auto vbo = std::make_shared<SunsetEngine::VertexBuffer>(vertices.data(), vertices.size(), sizeof(Vertex));
-    vbo->SetLayout({SunsetEngine::BufferElement{SunsetEngine::ShaderDataType::Float3, "position"}});
+        auto vbo = std::make_shared<SunsetEngine::VertexBuffer>(vertices.data(), vertices.size(), sizeof(Vertex));
+        vbo->SetLayout({SunsetEngine::BufferElement{SunsetEngine::ShaderDataType::Float3, "position"}});
 
-    auto ebo = std::make_shared<SunsetEngine::IndiceBuffer>(indices);
+        auto ebo = std::make_shared<SunsetEngine::IndiceBuffer>(indices);
 
-    auto vao = std::make_unique<SunsetEngine::VertexArray>();
-    vao->AddVertexBuffer(*vbo);
-    vao->AddIndexBuffer(*ebo);
+        auto vao = std::make_unique<SunsetEngine::VertexArray>();
+        vao->AddVertexBuffer(*vbo);
+        vao->AddIndexBuffer(*ebo);
 
-    m_Drawable->m_Mesh = std::make_unique<SunsetEngine::Mesh>(vao);
-    m_Drawable->m_Mesh->m_VertexBuffer = vbo;
-    m_Drawable->m_Mesh->m_IndiceBuffer = ebo;
+        m_Drawable->m_Mesh = std::make_unique<SunsetEngine::Mesh>(vao);
+        m_Drawable->m_Mesh->m_VertexBuffer = vbo;
+        m_Drawable->m_Mesh->m_IndiceBuffer = ebo;
+    }
+
+    /****************/
+    /*Cloud drawable*/
+    /****************/
+
+    {
+        CloudDrawable = std::make_unique<SunsetEngine::Drawable>();
+
+        auto shader = std::make_shared<SunsetEngine::Shader>("SunsetCraft/Shaders/Cloud.vert", "SunsetCraft/Shaders/Cloud.frag");
+        CloudDrawable->m_Material->m_Shader = shader;
+
+        auto vbo = std::make_shared<SunsetEngine::VertexBuffer>(CloudVertices.data(), CloudVertices.size(), sizeof(glm::vec3));
+        vbo->SetLayout({SunsetEngine::BufferElement{SunsetEngine::ShaderDataType::Float3, "position"}});
+
+        auto ebo = std::make_shared<SunsetEngine::IndiceBuffer>(CloudIndices);
+
+        auto vao = std::make_unique<SunsetEngine::VertexArray>();
+        vao->AddVertexBuffer(*vbo);
+        vao->AddIndexBuffer(*ebo);
+
+        CloudDrawable->m_Mesh = std::make_unique<SunsetEngine::Mesh>(vao);
+        CloudDrawable->m_Mesh->m_VertexBuffer = vbo;
+        CloudDrawable->m_Mesh->m_IndiceBuffer = ebo;
+    }
+
+
 }
 
 SkyCube::~SkyCube()
 {
+    CloudDrawable.reset();
     LOG("SunsetCraft", info, "SkyDome destroyed")
 }
 
 void SkyCube::Draw() const
 {
     SunsetEngine::RenderCommande::Submit(*m_Drawable);
+    SunsetEngine::RenderCommande::Submit(*CloudDrawable);
 }
